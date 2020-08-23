42c01245b3c782d0c432ed07bcfb594902bb87bf
Aviral Khandelwal <aviralkhandelwal15@gmail.com>
2020-08-24 02:15:31 +0530

    Add files via upload

---
+++ android/app/src/main/java/com/shopping/MainActivity.java
@@ -0,0 +1,31 @@
+package com.shopping;
+
+import com.facebook.react.ReactActivity;
+
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
+import android.content.Intent;
+import android.content.res.Configuration;
+
+public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+      public void onConfigurationChanged(Configuration newConfig) {
+      super.onConfigurationChanged(newConfig);
+      Intent intent = new Intent("onConfigurationChanged");
+      intent.putExtra("newConfig", newConfig);
+      sendBroadcast(intent);
+    }
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "shopping";
+  }
+}
